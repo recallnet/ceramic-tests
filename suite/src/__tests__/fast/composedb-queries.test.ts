92dc1bddd68fe2529f1eb7c9923f3b8536731e59
JulissaDantes <julissadcj@gmail.com>
2023-11-16 15:40:57 -0400

    feat:  Add tests queries

--- suite/src/__tests__/fast/composedb-queries.test.ts
+++ suite/src/__tests__/fast/composedb-queries.test.ts
@@ -1,7 +1,7 @@
 import {afterAll, beforeAll, describe, expect, test} from '@jest/globals'
 import * as helpers from '../../utils/dynamoDbHelpers.js'
 import { ComposeClient }from '@composedb/client'
-
+import { definition } from   './__generated__/definition.js'
 
 // Environment variables
 const ComposeDbUrls = String(process.env.COMPOSEDB_URLS).split(',')
@@ -11,30 +11,68 @@ const ComposeDbUrls = String(process.env.COMPOSEDB_URLS).split(',')
 ///////////////////////////////////////////////////////////////////////////////
 
 describe('create/update/queries on ComposeDB Models', () => {
-    beforeAll(async () => {})
-    afterAll(async () => await helpers.cleanup())
-    const definition: RuntimeCompositeDefinition = //TODO
-    
-    const compose = new ComposeClient({ ceramic: ComposeDbUrls[0], definition })
+    beforeAll(async () => {})//TODO
+    afterAll(async () => await helpers.cleanup())//TODO
 
+    const compose = new ComposeClient({ ceramic: ComposeDbUrls[0], definition })
+// TODO add test criteria e.g 'expect' keyword
     test('test create', async () => {
-       //create model 
-       // only create it if it doesn't exist?
+        await compose.executeQuery(`mutation {
+            createGenericModel(input: {
+                content: {
+                    numericalField: 42,
+                    textField: "Sample Text",
+                    booleanField: true
+                }
+            }) 
+            {
+                document {
+                    numericalField
+                    textField
+                    booleanField
+                  }
+            }
+          }`)
 
     })
 
     test('test update', async () => {
-       //update model
+        //TODO get id from prev test
+        //TODO make update data be a variable to test against it at the end
+       await compose.executeQuery(`mutation UpdateGenericModel {
+        updateGenericModel(
+          input: { 
+            id: "${id}",
+            content: {
+              numericalField: 42,
+              textField: "Updated Text",
+              booleanField: true  
+            }
+          }
+        ) {
+          document {
+            numericalField
+            textField
+            booleanField
+          }
+        }
+      }`)
     })
 
     test('test query', async () => {
-       //read model
+        // TODO make search param a variable
        await compose.executeQuery(`
-       query {
-           viewer {
-           id
-           }
-       }
-       `)
+       query numericalFieldFiltered {
+        genericModelIndex(first: 1, filters: { where: {numericalField: {equalTo: 2} } }) {
+              edges {
+            node {
+                id
+                numericalField
+                textField
+                booleanField
+            }
+          }
+        }
+      }`)
     })
 })
