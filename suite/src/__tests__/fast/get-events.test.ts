8d9a4d0e52d6bca790a0e9eaa206b443d2fb0062
Samika Kashyap <samikas@Samikas-MacBook-Pro.local>
2024-02-06 23:27:03 +0530

    fix: code smell

--- suite/src/__tests__/fast/get-events.test.ts
+++ suite/src/__tests__/fast/get-events.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from '@jest/globals'
 import fetch from 'cross-fetch'
 import { generateRandomEventId, generateRandomEvent } from '../../utils/rustCeramicHelpers'
 
-const CeramicUrls = String(process.env.CERAMIC_URLS).split(',')
+// const CeramicUrls = String(process.env.CERAMIC_URLS).split(',')
 
 async function postEvent(url: string, event: any) {
   let response = await fetch(url + '/ceramic/events', {
@@ -24,7 +24,8 @@ async function getEventData(url: string, eventId: string) {
 }
 
 describe('rust-ceramic e2e test', () => {
-  const ceramicUrl = CeramicUrls[0]
+  // const ceramicUrl = CeramicUrls[0]
+  const ceramicUrl = "http://127.0.0.1:5001"
   test('post and get event data, success', async () => {
     const eventId = generateRandomEventId()
     const event = generateRandomEvent(eventId)
@@ -47,6 +48,5 @@ describe('rust-ceramic e2e test', () => {
     expect(getResponse.status).toEqual(404)
     expect(responseText).toContain('Event not found')
     expect(responseText).toContain(eventId)
-    expect(getResponse.statusText).toContain('Bad Request')
   })
 })
