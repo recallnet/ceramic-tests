54fce43b35eed88431ff6a8c555a4ddcbc4db57e
Samika Kashyap <samikas@Samikas-MacBook-Pro.local>
2024-02-06 15:53:49 +0530

    fix: format

--- suite/src/__tests__/fast/get-events.test.ts
+++ suite/src/__tests__/fast/get-events.test.ts
@@ -4,17 +4,16 @@ import { generateRandomEventId, generateRandomEvent } from '../../utils/rustCera
 
 const CeramicUrls = String(process.env.CERAMIC_URLS).split(',')
 
-
 async function postEvent(url: string, event: any) {
   let response = await fetch(url + '/ceramic/events', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json', 
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(event),
   })
   if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+    throw new Error(`HTTP error! status: ${response.status}`)
   }
 }
 
@@ -25,30 +24,29 @@ async function getEventData(url: string, eventId: string) {
 }
 
 describe('rust-ceramic e2e test', () => {
-  const ceramicUrl = CeramicUrls[0] 
-  // const ceramicUrl = "http://127.0.0.1:5001"
+  const ceramicUrl = CeramicUrls[0]
   test('post and get event data, success', async () => {
-    const eventId = generateRandomEventId();
-    const event = generateRandomEvent(eventId);
+    const eventId = generateRandomEventId()
+    const event = generateRandomEvent(eventId)
     // publishing the event to rust-ceramic
     await postEvent(ceramicUrl, event)
     // fetching the event from its event-id from rust-ceramic
     const getResponse = await getEventData(ceramicUrl, eventId)
-    expect(getResponse.status).toEqual(200);
-    expect(await (getResponse.json())).toEqual({
-      "eventId": eventId,
-      "eventData": event.eventData
+    expect(getResponse.status).toEqual(200)
+    expect(await getResponse.json()).toEqual({
+      eventId: eventId,
+      eventData: event.eventData,
     })
   })
 
   test('get event data for non-existing event', async () => {
-    const eventId = generateRandomEventId();
+    const eventId = generateRandomEventId()
     // fetching the event from its event-id from rust-ceramic
     const getResponse = await getEventData(ceramicUrl, eventId)
-    const responseText = await getResponse.text();
-    expect(getResponse.status).toEqual(400);
-    expect(responseText).toContain('Event not found');
-    expect(responseText).toContain(eventId);
-    expect(getResponse.statusText).toContain('Bad Request');
+    const responseText = await getResponse.text()
+    expect(getResponse.status).toEqual(400)
+    expect(responseText).toContain('Event not found')
+    expect(responseText).toContain(eventId)
+    expect(getResponse.statusText).toContain('Bad Request')
   })
 })
--- suite/src/utils/rustCeramicHelpers.ts
+++ suite/src/utils/rustCeramicHelpers.ts
@@ -1,28 +1,26 @@
 import { randomCID, EventID, StreamID } from '@ceramicnetwork/streamid'
 import { base16 } from 'multiformats/bases/base16'
-import { CARFactory } from "cartonne"
+import { CARFactory } from 'cartonne'
 import { base64 } from 'multiformats/bases/base64'
 import * as random from '@stablelib/random'
 
-export function generateRandomEventId (): string {
-    const modelID = new StreamID('model', randomCID())
-      const eventId =  base16.encode(EventID.createRandom(
-        "dev-unstable",
-        0,
-        {
-          separatorKey: 'model',
-          separatorValue: modelID.toString(),
-        }
-      ).bytes);
-    return eventId;
+export function generateRandomEventId(): string {
+  const modelID = new StreamID('model', randomCID())
+  const eventId = base16.encode(
+    EventID.createRandom('dev-unstable', 0, {
+      separatorKey: 'model',
+      separatorValue: modelID.toString(),
+    }).bytes,
+  )
+  return eventId
 }
 
-export function generateRandomEvent (eventId: string): any {
-    const carFactory = new CARFactory();
-    const car = carFactory.build().asV1();
-    car.put({ data: base64.encode(random.randomBytes(512)) }, { isRoot: true });
-    return {
-      "eventId": eventId,
-      "eventData": car.toString('base64'),
-    };
+export function generateRandomEvent(eventId: string): any {
+  const carFactory = new CARFactory()
+  const car = carFactory.build().asV1()
+  car.put({ data: base64.encode(random.randomBytes(512)) }, { isRoot: true })
+  return {
+    eventId: eventId,
+    eventData: car.toString('base64'),
   }
+}
