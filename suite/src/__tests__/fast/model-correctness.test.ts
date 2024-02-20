db44e17bcc8a25737355744241e7493a62c76f9d
Samika Kashyap <samikas@Samikas-MacBook-Pro.local>
2024-02-21 02:50:06 +0530

    fix: completed happy test flow
    fix: remove debug logs
    
    fix: add lockfile
    
    fix: package json typo
    
    fix: removing changes from dev dependencies:
    
    fix: pnpm lock
    
    fix: upgrade to node 20
    
    fix: removed composedb imports
    
    fix: remove unused import
    
    fix: just...
    
    fix: formating and more
    
    fix: removed constant modelId
    
    fix: update rust.yaml for hermetic tests env
    
    fix: give perf tests more resources (#100)
    
    fix: add env for recon
    
    fix: changed ceramic image to latest dev image
    
    fix: change yaml debug
    
    fix: change aimge
    
    fix: revert to stable builds
    
    fix: hail mary
    
    fix:  blah
    
    fix: added logs
    
    fix: blah
    
    fix: happy flow test

--- suite/src/__tests__/fast/model-correctness.test.ts
+++ suite/src/__tests__/fast/model-correctness.test.ts
@@ -1,58 +1,56 @@
 import { describe, test, beforeAll, expect } from '@jest/globals'
 import { newCeramic } from '../../utils/ceramicHelpers.js'
-import { createDid, splitDidsToList } from '../../utils/didHelper.js'
+import { createDid } from '../../utils/didHelper.js'
 import { StreamID } from '@ceramicnetwork/streamid'
-import {} from '@composedb/cli'
-import {Model} from '@ceramicnetwork/stream-model'
-import {ModelInstanceDocument} from '@ceramicnetwork/stream-model-instance'
+import { Model } from '@ceramicnetwork/stream-model'
+import { ModelInstanceDocument } from '@ceramicnetwork/stream-model-instance'
 import { newModel, basicModelDocumentContent } from '../../models/modelConstants'
 import { CeramicClient } from '@ceramicnetwork/http-client'
+import { CommonTestUtils as TestUtils } from '@ceramicnetwork/common-test-utils'
+import { utilities } from '../../utils/common.js'
 
+const delay = utilities.delay
 const ComposeDbUrls = String(process.env.COMPOSEDB_URLS).split(',')
-// TODO : Update to correct modelId
-// TODO : Will not result in the same stream id , have an arry per env
-let modelId = "kjzl6hvfrbw6cabolodt3tflk3jdumv7dmgv8qss5jvjzcd7kck5drjkaun0bx1"
-let seed = String(process.env.COMPOSEDB_ADMIN_DID_SEEDS)
+// TODO : Update to the modelId created manually on all envs, if id cannot be the same then create an array for different envs
+const adminSeeds = String(process.env.COMPOSEDB_ADMIN_DID_SEEDS).split(',')
+const nodeSyncWaitTimeSec = 2
+
 describe('Model Integration Test', () => {
   let ceramicNode1: CeramicClient
-  let streamId = StreamID.fromString(modelId);
+  let ceramicNode2: CeramicClient
+  let modelId: StreamID
   beforeAll(async () => {
-    let did = await createDid(splitDidsToList(seed)[1]);
-    console.log(ComposeDbUrls[0])
+    console.log('Reading admin seeds:', adminSeeds)
+    const did = await createDid(adminSeeds[0])
     ceramicNode1 = await newCeramic(ComposeDbUrls[0], did)
-    // let ceramicNode2 = await newCeramic(ComposeDbUrls[1], did)
-    const indexedModels = await ceramicNode1.admin.getIndexedModels();
-    console.log(indexedModels);
-
-    if(!indexedModels.includes(streamId)){
-        let model = await Model.create(ceramicNode1, newModel);
-        streamId = model.id;
-    }
-
-    // index the model on node 1
-
-   // wait for some time TESTUTILS      
-//    await ceramicNode2.admin.startIndexingModels([streamId]);
+    ceramicNode2 = await newCeramic(ComposeDbUrls[1], did)
+    console.log('Created ceramic client for node 1', ceramicNode1)
+    console.log('Created ceramic client for node 2', ceramicNode2)
+    let model = await Model.create(ceramicNode1, newModel)
+    console.log('Creating model on node1 ', model)
+    TestUtils.waitForConditionOrTimeout(async () =>
+        ceramicNode1
+          .loadStream(model.id)
+          .then((_) => true)
+          .catch((_) => false),
+    )
+    await ceramicNode1.admin.startIndexingModels([model.id])
+    console.log('Indexing model on node1')
+    await ceramicNode2.admin.startIndexingModels([model.id])
+    console.log('Indexed model on Node 2', model.id)
+    modelId = model.id
   })
 
   test('Create a ModelInstanceDocument on one node and read it from another', async () => {
-    const modelInstanceDocumentMetdata = {model: streamId}
-    // console.log(modelInstanceDocumentMetdata)
-    const document = await ModelInstanceDocument.create(ceramicNode1, basicModelDocumentContent, modelInstanceDocumentMetdata);
-    // console.log(document)
-    await new Promise(resolve => setTimeout(resolve, 5000));
-
-
-    let indexedModels = await ceramicNode1.admin.getIndexedModels();
-    // let document2 = await ceramicNode2.index.query()
-    // or
-    // ModelInstanceDocument.load(ceramicNode2, basicModelDocumentContent, modelInstanceDocumentMetdata)
-
-
-    // TODO_2 : add assert that indexedModels list should contain document.id
-    expect(indexedModels).toContain(document.id.toString());
-    expect(document.id).not.toBeUndefined();
-    
+    const modelInstanceDocumentMetdata = { model: modelId }
+    const document1 = await ModelInstanceDocument.create(
+      ceramicNode1,
+      basicModelDocumentContent,
+      modelInstanceDocumentMetdata,
+    )
+    // We have to wait for some time for sync to happen
+    await delay(nodeSyncWaitTimeSec)
+    const document2 = await ModelInstanceDocument.load(ceramicNode2, document1.id)
+    expect(document2.id).toEqual(document1.id)
   })
 })
-
--- suite/src/utils/didHelper.ts
+++ suite/src/utils/didHelper.ts
@@ -1,24 +1,14 @@
-// import { randomBytes } from 'crypto'
 import { DID } from 'dids'
 import { Ed25519Provider } from 'key-did-provider-ed25519'
 import KeyDidResolver from 'key-did-resolver'
 import { randomBytes } from '@stablelib/random'
 import * as uint8arrays from 'uint8arrays'
 
-
-  export function splitDidsToList(input: string): string[] {
-    // Split the input string by commas and trim whitespace from each resulting string
-    return input.split(',').map(s => s.trim());
-  }
-
-  export async function createDid(seed?: string): Promise<DID> {
-    const digest = seed ? uint8arrays.fromString(seed, 'base16') : randomBytes(32)
-    console.log(digest)
-    const provider = new Ed25519Provider(digest)
-    const resolver = KeyDidResolver.getResolver()
-    const did = new DID({ provider, resolver })
-    console.log("This is the did", did)
-    await did.authenticate()
-    console.log('Authenticated did', did.id)
-    return did
-  }
+export async function createDid(seed?: string): Promise<DID> {
+  const digest = seed ? uint8arrays.fromString(seed, 'base16') : randomBytes(32)
+  const provider = new Ed25519Provider(digest)
+  const resolver = KeyDidResolver.getResolver()
+  const did = new DID({ provider, resolver })
+  await did.authenticate()
+  return did
+}
